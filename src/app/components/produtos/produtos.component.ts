17084b439c512661c866524559e91c8cb11b5bc9
Anderson freires <andersonfreires1979@gmail.com>
2026-05-09 21:27:45 -0300

    Atualização de imagens- Parte 4

--- src/app/components/produtos/produtos.component.ts
+++ src/app/components/produtos/produtos.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ProdutoService } from '../../services/produto.service';
 import { Produto } from '../../models/produto.model';
 
 @Component({
   selector: 'app-produtos',
-  templateUrl: './produtos.component.html'
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './produtos.component.html',
+  styleUrls: ['./produtos.component.css']
 })
 export class ProdutosComponent implements OnInit {
 
@@ -12,13 +16,13 @@ export class ProdutosComponent implements OnInit {
 
   constructor(private produtoService: ProdutoService) {}
 
-usarImagemLocal(event: any, imageUrl: string) {
-  event.target.src = 'assets/imagens/placeholder.jpg';
-}
-adicionar(produto: Produto) {
-  console.log('Produto adicionado:', produto);
-}
+  usarImagemLocal(event: any, imageUrl: string) {
+    event.target.src = 'assets/imagens/placeholder.jpg';
+  }
 
+  adicionar(produto: Produto) {
+    console.log('Produto adicionado:', produto);
+  }
 
   ngOnInit(): void {
     this.produtoService.listar().subscribe({
