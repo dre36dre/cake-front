30a7d4fb63d64089fafef6ba76fc240c97d7334b
Anderson freires <andersonfreires1979@gmail.com>
2026-05-11 19:54:06 -0300

    Edição de produtos pelo Admin-Parte 8

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { bootstrapApplication, provideZoneChangeDetection } from '@angular/platform-browser';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
@@ -8,7 +8,6 @@ import { authInterceptor } from './app/interceptors/auth-interceptor';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideZoneChangeDetection(),
     provideHttpClient(withInterceptors([authInterceptor])),
     provideRouter(routes)
   ]
